888525653263836f319122b3a64053588af0e48e
Mayank Mishra <mmayank74567@gmail.com>
2021-05-18 09:39:47 +0530

    Newimage

--- convolutions/convolutions.py
+++ convolutions/convolutions.py
@@ -1,28 +1,30 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri May 14 09:00:37 2021
-
-@author: HP
-"""
 
 from skimage.exposure import rescale_intensity
 import numpy as np
 import argparse
 import cv2
 
-#the convolve function
+apo = argparse.ArgumentParser()
+apo.add_argument('-i','--image', required = True, help = "Path to the image you wish to convolve")
+args = vars(apo.parse_args())
 
-def convolve(img, kernel):
-    #establishing the dimensions of the image and kernel
-    (imgh, imgw) = img.shape[:2]
-    (kerh, kerw) = kernel.shape[:2]
+def convolution(img, kernel):
+    #adding padding to the image to not lose the spatial dimensions
     
-    #adding padding to the input image
-    #so that the spatial size is not reduced
+    img_h, img_w = img.shape[:2]
+    k_h, k_w = kernel.shape[:2]
     
-    padding = (kerw-1)//2
+    pad = (k_w - 1)//2
     
-    img = cv2.copyMakeBorder(img, padding, padding, padding, padding, cv2.BORDER_REPLICATE)
-    output = np.zeroes((imgh, imgw), dtype = "float32")
+    #adding this padding to the image
     
+    img = cv2.copyMakeBorder(img, pad, pad, pad, pad, cv2.BORDER_REPLICATE)
+    cv2.imshow("Image with padding",img)
+    cv2.waitKey(0)
+
+kernel = np.ones((3,3))
+print(kernel)
+img = cv2.imread(args["image"])
+img = cv2.resize(img, (500,400))
+convolution(img, kernel)   
     
